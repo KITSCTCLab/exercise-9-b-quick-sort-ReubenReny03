0dcad34967115bd92ff4089073d4eac2f568937c
Reuben Reny <91770238+ReubenReny03@users.noreply.github.com>
2022-10-06 10:35:41 +0530

    basic code

--- Main.py
+++ Main.py
@@ -1,8 +1,24 @@
 from typing import List
 
 def quick_sort(data, low, high) -> List[int]:
-    # Write code here
-
+    if low < high:
+        i = low
+        j = high-1
+        pivot = data[high]
+        
+        while i < j:
+            while i< high and data[i] < pivot:
+                i+=1
+            while j<low and data[j] > pivot:
+                j-=1
+            if i < j:
+                data[i], data[j] = data[j], data[i]
+                
+        if data[i] > pivot:
+            data[i], data[high] = data[high], data[i]
+        partision_pos = i
+        quick_sort(data,low,partision_pos-1)
+        quick_sort(data,partision_pos+1,high)
 
 input_data = input()
 data = []
